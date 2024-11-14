a3fee3b366e693f5f4c6cd6096977229ae1b753a
Kushagra Awasthi <Kushagra111awasthi@gmail.com>
2024-11-14 21:32:59 +0530

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import SignOut from './pages/SignUp'
 import About from './pages/About'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Header from './components/Header'
 
 export default function App() {
   return (<BrowserRouter>
+  <Header/>
   <Routes>
     <Route path="/" element = {<Home/>}/>    
     <Route path="/About" element = {<About/>}/>    
-    <Route path="/SignOut" element = {<SignUp/>}/>    
-    <Route path="/SignIn" element = {<SignIn/>}/>    
+    <Route path="/Sign-up" element = {<SignUp/>}/>    
+    <Route path="/Sign-In" element = {<SignIn/>}/>    
 
   </Routes>
   </BrowserRouter>)
