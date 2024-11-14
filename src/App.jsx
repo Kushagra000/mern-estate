a4369d65dc9017b68772746edd08cdb41ce61f95
Kushagra Awasthi <Kushagra111awasthi@gmail.com>
2024-11-14 20:21:34 +0530

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignOut from './pages/SignUp'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 export default function App() {
-  return (
-    <div className='bg-red-600'>
-      App      
-    </div>
-  )
+  return (<BrowserRouter>
+  <Routes>
+    <Route path="/" element = {<Home/>}/>    
+    <Route path="/About" element = {<About/>}/>    
+    <Route path="/SignOut" element = {<SignUp/>}/>    
+    <Route path="/SignIn" element = {<SignIn/>}/>    
+
+  </Routes>
+  </BrowserRouter>)
 }
