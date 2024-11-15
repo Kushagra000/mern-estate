e13d416effcb86dd4c4040c8c1276babe95b8a82
Kushagra Awasthi <Kushagra111awasthi@gmail.com>
2024-11-15 10:55:16 +0530

    user model

---
+++ api/models/user.model.js
@@ -0,0 +1,18 @@
+import { Timestamp } from "bson";
+import mongoose from "mongoose";
+
+const userSchema= new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,       
+    },
+    password:{
+        type:String,
+        required:true, 
+    }
+},{timestamps:ture});
+
+const User = mongoose.model('User',userSchema);
+
+export default User;
