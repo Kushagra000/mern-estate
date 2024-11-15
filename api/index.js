7cdf9012526ac6f2ae1af4fbc5b800135e896f29
Kushagra Awasthi <Kushagra111awasthi@gmail.com>
2024-11-15 17:48:32 +0530

    Middleware

--- api/index.js
+++ api/index.js
@@ -18,4 +18,14 @@ app.listen(3000, ()=>{
 });
 
 app.use("/api/user",userRouter);
-app.use("/api/auth",authRouter)
+app.use("/api/auth",authRouter);
+
+app.use((err, req, res, next)=>{
+    const stausCode = err.statusCode || 500;
+    const message = err.message || "Internal srver Error";
+    return res.status(stausCode).json({
+        success: false,
+        stausCode,
+        message
+    });
+})
